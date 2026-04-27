36bb3c52f335dd2fb6b722a7155c3999e3522219
Stanislav Žiačik <ziacik4@stud.uniza.sk>
2026-04-27 22:54:44 +0200

    Implementácia PresunDoCakarne.java

--- src/agents/agenturgentu/continualassistants/PresunDoCakarne.java
+++ src/agents/agenturgentu/continualassistants/PresunDoCakarne.java
@@ -1,6 +1,9 @@
 package agents.agenturgentu.continualassistants;
 
 import OSPABA.*;
+import entity.Pacient;
+import generatory.ContinousGenerator;
+import generatory.TriangularDistribution;
 import simulation.*;
 import agents.agenturgentu.*;
 import OSPABA.Process;
@@ -8,7 +11,10 @@ import OSPABA.Process;
 //meta! id="107"
 public class PresunDoCakarne extends OSPABA.Process
 {
-	public PresunDoCakarne(int id, Simulation mySim, CommonAgent myAgent)
+    private TriangularDistribution pesoGenerator;
+    private ContinousGenerator sanitkaGenerator;
+
+    public PresunDoCakarne(int id, Simulation mySim, CommonAgent myAgent)
 	{
 		super(id, mySim, myAgent);
 	}
@@ -18,11 +24,24 @@ public class PresunDoCakarne extends OSPABA.Process
 	{
 		super.prepareReplication();
 		// Setup component for the next replication
+        MySimulation sim = (MySimulation) mySim();
+
+        this.pesoGenerator = new TriangularDistribution(sim.getSeedGenerator(),120,300,150);
+
+        this.sanitkaGenerator = new ContinousGenerator(90,200, sim.getSeedGenerator());
 	}
 
 	//meta! sender="AgentUrgentu", id="108", type="Start"
 	public void processStart(MessageForm message)
 	{
+        if (message.lastPost() == MessageForm.PostType.start) {
+            hold(vygenerujCasPresunu((MyMessage) message), message);
+            return;
+        }
+
+        if (message.lastPost() == MessageForm.PostType.hold) {
+            assistantFinished(message);
+        }
 	}
 
 	//meta! userInfo="Process messages defined in code", id="0"
@@ -55,5 +74,18 @@ public class PresunDoCakarne extends OSPABA.Process
 	{
 		return (AgentUrgentu)super.myAgent();
 	}
+
+    private double vygenerujCasPresunu(MyMessage msg)
+    {
+        if (msg.getPacient().getTyp() == Pacient.TypPacienta.PESO) {
+            return pesoGenerator.nextValue();
+        }
+
+        if (msg.getPacient().getTyp() == Pacient.TypPacienta.SANITKA) {
+            return sanitkaGenerator.nextDouble();
+        }
+
+        throw new IllegalStateException("Neznamy typ pacienta");
+    }
 
 }
