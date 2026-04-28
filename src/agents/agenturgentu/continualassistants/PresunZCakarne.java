623a4112f8399b0833188377a8202aedf04b1e05
Stanislav Žiačik <ziacik4@stud.uniza.sk>
2026-04-28 11:17:14 +0200

    Po pregenerovani - Presun OdchoduPacienta do Agenta urgentu (Zabudol som pridať PresunZCakarne)

---
+++ src/agents/agenturgentu/continualassistants/PresunZCakarne.java
@@ -0,0 +1,59 @@
+package agents.agenturgentu.continualassistants;
+
+import OSPABA.*;
+import simulation.*;
+import agents.agenturgentu.*;
+import OSPABA.Process;
+
+//meta! id="114"
+public class PresunZCakarne extends OSPABA.Process
+{
+	public PresunZCakarne(int id, Simulation mySim, CommonAgent myAgent)
+	{
+		super(id, mySim, myAgent);
+	}
+
+	@Override
+	public void prepareReplication()
+	{
+		super.prepareReplication();
+		// Setup component for the next replication
+	}
+
+	//meta! sender="AgentUrgentu", id="115", type="Start"
+	public void processStart(MessageForm message)
+	{
+	}
+
+	//meta! userInfo="Process messages defined in code", id="0"
+	public void processDefault(MessageForm message)
+	{
+		switch (message.code())
+		{
+		}
+	}
+
+	//meta! userInfo="Generated code: do not modify", tag="begin"
+	@Override
+	public void processMessage(MessageForm message)
+	{
+		switch (message.code())
+		{
+		case Mc.start:
+			processStart(message);
+		break;
+
+		default:
+			processDefault(message);
+		break;
+		}
+	}
+	//meta! tag="end"
+
+	@Override
+	public AgentUrgentu myAgent()
+	{
+		return (AgentUrgentu)super.myAgent();
+	}
+
+}
